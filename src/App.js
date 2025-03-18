fd4d507fbb058f7adb3565277592145137615fba
kellyxo <dude7037@gmail.com>
2025-03-18 16:56:09 -0500

    added fire base to app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,33 @@ import Profile from './components/Profile';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import './App.css';
 import './ModernTheme.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXejjWN4iBlaHeu-aFR4avlYclIeGB_T4",
+  authDomain: "memory-lane-1efeb.firebaseapp.com",
+  projectId: "memory-lane-1efeb",
+  storageBucket: "memory-lane-1efeb.firebasestorage.app",
+  messagingSenderId: "497823584629",
+  appId: "1:497823584629:web:14f14faa4e02548eb7833c",
+  measurementId: "G-F6Z2TT2K3T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 function App() {
   const [currentUser, setCurrentUser] = useState(localStorage.getItem('currentUser'));
   const [currentTheme, setCurrentTheme] = useState(localStorage.getItem('theme') || 'light');
-
+  
   useEffect(() => {
     if (currentUser) {
       localStorage.setItem('currentUser', currentUser);

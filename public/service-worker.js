b33c8385b7a3cb2952e31474d3b2e9ba4800dddc
kellyxo <dude7037@gmail.com>
2025-03-30 03:54:58 -0500

    1.2\

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // public/service-worker.js
 
-const APP_VERSION = 'v1.1.1';
+const APP_VERSION = 'v1.2';
 
 // Names for our caches with versioning
 const STATIC_CACHE = `memorylane-static-${APP_VERSION}`;

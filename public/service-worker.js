cd34926f537f1d2ff0d9d918848a0a48f2a45cf7
kellyxo <dude7037@gmail.com>
2025-04-21 13:43:33 -0500

    Added comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // public/service-worker.js
 
-const APP_VERSION = 'v1.7';
+const APP_VERSION = 'v1.7.1';
 
 // Names for our caches with versioning
 const STATIC_CACHE = `memorylane-static-${APP_VERSION}`;
